1517d874f9bd901a41b0e5c088ee6bc30493471f
zengyang <759913558@qq.com>
2017-08-28 15:39:12 +0800

    cook添加消息推送提醒音

--- api/module/DBHelper.js
+++ api/module/DBHelper.js
@@ -8,7 +8,7 @@ function create() {
         host     : 'localhost',
         user     : 'root',
         password : '',
-        database : 'delicious'
+        database : 'zyy'
     })
     connection.connect();
 }
--- api/module/ProductsDB.js
+++ api/module/ProductsDB.js
@@ -6,7 +6,7 @@ connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'delicious'
+    database : 'zyy'
 })
 connection.connect();
 
