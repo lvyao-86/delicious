e2d9ec3eb679821bf05cc05a5ff82b696007de2f
wy <418631670@qq.com>
2017-08-30 09:44:47 +0800

    charts样式修改

--- web/src/components/charts/charts.vue
+++ web/src/components/charts/charts.vue
@@ -1,6 +1,6 @@
 <template>
   <!--为echarts准备一个具备大小的容器dom-->
-  <div id="charts" style="width: 900px;height: 500px;"></div>
+  <div id="charts" style="width: 600px;height: 350px;"></div>
 </template>
 <script>
     import echarts from 'echarts'
@@ -20,30 +20,67 @@
             drawPie(id, arr1, arr2){
                 this.charts = echarts.init(document.getElementById(id))
                 this.charts.setOption({
-            title: {
-                text: '菜式销量排行'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: arr1,
-                axisLabel: {  
-                    interval:0,  
-                    rotate:40    
-                   /* formatter:function(value){  
-                       return value.split("").join("\n");  
-                    }  */  
-                }                                                                                   
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: arr2
-            }]
-        });
+                    title: {
+                        text: '菜式销量排行',
+                        subtext: 'Sales Example',
+                        x: 'center'
+                    },
+                    tooltip: {},
+                     toolbox: {
+                        show: true,
+                        feature: {
+                            restore: {show: true},
+                        }
+                    },
+                    /*legend: {
+                        data:['销量']
+                    },*/
+                    xAxis: {
+                        data: arr1,
+                        axisLabel: {  
+                            interval:0,  
+                            formatter: function(value) {  
+                                debugger  
+                                var ret = "";//拼接加\n返回的类目项  
+                                var maxLength = 2;//每项显示文字个数  
+                                var valLength = value.length;//X轴类目项的文字个数  
+                                var rowN = Math.ceil(valLength / maxLength); //类目项需要换行的行数  
+                                if (rowN > 1)//如果类目项的文字大于3,  
+                                {  
+                                    for (var i = 0; i < rowN; i++) {  
+                                        var temp = "";//每次截取的字符串  
+                                        var start = i * maxLength;//开始截取的位置  
+                                        var end = start + maxLength;//结束截取的位置  
+                                        temp = value.substring(start, end) + "\n";  
+                                        ret += temp; //凭借最终的字符串  
+                                    }  
+                                    return ret;  
+                                }  
+                                else {  
+                                    return value;  
+                                }  
+                            },
+                            textStyle: {
+                               fontSize: 10
+                            }  
+                        }                                                                                   
+                    }, 
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: arr2,
+                        itemStyle: {
+                            normal: {
+                                color: function (params){
+                                    var colorList = ['#C1232B','#B5C334','#FCCE10','#E87C25','#27727B','#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD','#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0','rgb(164,205,238)','rgb(42,170,227)'];
+                                    return colorList[params.dataIndex];
+                                },
+                                
+                            }
+                        }
+                    }],
+                });
             }
         },
       //调用
@@ -76,7 +113,7 @@
         list-style: none;
     }
     #charts{
-        margin: 0 auto;
+        /*margin: 0 auto;*/
         margin-top: 20px;
     }
 </style>
